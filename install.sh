e1c1261157d4c1ab417c84168f3385e6def5747d
Balder W. Holst <balderwh@gmail.com>
2023-01-20 18:54:42 +0100

    added install script

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-git >/dev/null && echo "in git repo"
+dir=$(dirname $0)
+
+
+
+# if the repo exist
+[[ -d "$dir/.git" ]] && [[ ! -d "$dir/.obsidian" ]] && \
+    git clone https://github.com/BalderHolst/uni-notes-settings "$dir/.obsidian" && \
+    echo "Added settings." && exit 0
+
+# if script is run by itself
+[[ ! -d "$dir/.git" ]] && \
+    git clone https://github.com/BalderHolst/uni-notes noter && \
+    git clone https://github.com/BalderHolst/uni-notes-settings "$dir/.obsidian" && \
+    echo "Cloned notes and added settings." && exit 0
+
+echo "Nothing to do."
+exit 0
