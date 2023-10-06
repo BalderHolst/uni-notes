70e1f83136187e048fe8339f35f49d3bd3c18ab0
Balder W. Holst <balderwh@gmail.com>
2023-10-06 15:57:03 +0200

    Revert "vault backup: 2023-09-28 08:24:37"
    This reverts commit 4e7d1d620c3c35d33f330427bf7e4f818d576dfd.

---
+++ .obsidian.vimrc
@@ -0,0 +1,8 @@
+" Quickly remove search highlights
+nmap <F9> :nohl
+
+" Yank to system clipboard
+set clipboard=unnamed
+
+" nmap j gj
+" nmap k gk
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+dir=$(dirname $0)
+
+dir_name="notes"
+
+
+# if the repo exist
+[[ -d "$dir/.git" ]] && [[ ! -d "$dir/.obsidian" ]] && \
+    git clone https://github.com/BalderHolst/uni-notes-settings "$dir/.obsidian" && \
+    echo "Added settings." && exit 0
+
+# if script is run by itself
+[[ ! -d "$dir/.git" ]] && \
+    git clone https://github.com/BalderHolst/uni-notes "$dir_name" && \
+    git clone https://github.com/BalderHolst/uni-notes-settings "$dir/$dir_name/.obsidian" && \
+    echo "Cloned notes and added settings." && exit 0
+
+echo "Nothing to do."
+exit 0
