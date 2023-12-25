821b1972ea9a3d5c9fab77eae8f7ca905764aeb6
BalderHolst <balderwh@gmail.com>
2023-12-25 23:11:01 +0100

    add `--classmates` to install script

--- install.sh
+++ install.sh
@@ -5,33 +5,63 @@ dir_name="notes"
 
 done_something=0
 
+info () {
+    echo -e "\u001b[36;1m""$@""\u001b[0m"
+}
+
 # if script is run by itself
 [[ ! -d "$dir/.git" ]] && {
+    info "Cloning notes..."
     git clone git@github.com:BalderHolst/uni-notes "$dir_name"
     dir="$dir/$dir_name"
     done_something=1
-    echo "Cloned notes."
 }
 
 # if the repo exist
 [[ -d "$dir/.git" ]] && [[ ! -d "$dir/.obsidian" ]] && {
+    info "Adding settings..."
     git clone git@github.com:BalderHolst/uni-notes-settings "$dir/.obsidian"
-    git -C "$dir" update-index --assume-unchanged ".obsidian"
     done_something=1
-    echo "Added settings."
 }
+
+CLASSMATES_DIR="classmates"
 
 # Function for cloning classmates's vaults
 clone_classmate_vault ()
 {
-    [[ ! -d "$dir/" ]]
     name="$1"
     url="$2"
+    path="$dir/$CLASSMATES_DIR/$name"
+
+    [[ ! -d "$dir/$CLASSMATES_DIR" ]] && \
+        mkdir "$dir/$CLASSMATES_DIR" && \
+        info "Created '$CLASSMATES_DIR' directory."
+
+    # Try to pull changes if the repo is already cloned
+    [[ -d "$path" ]] && {
+        if [[ -d "$path/.git" ]]; then
+            info "Pulling $name's notes..."
+            git -C "$path" pull
+        else
+            info "`$path` is not a git repo. Skipping..."
+        fi
+        return
+    }
+    info "Cloning $name's notes..."
+    git clone "$url" "$path"
 }
 
+# Clone classmates's notes if the `--classmates` flag is provided
 [[ "$1" = "--classmates" ]] && {
-    
+    clone_classmate_vault "Kasper" "https://github.com/TheJoboReal/Noter" & \
+    clone_classmate_vault "Jacob" "https://github.com/Jack-The-Dane/UNI_Notes"
+    sleep 0.5 # Wait for prints to happen as to not mess up prompt1
     done_something=1
 }
 
-[[ $done_something = 0 ]] && echo "Nothing to do."
+if [[ $done_something = 1 ]]; then
+    info "Done!"
+else
+    info "Nothing to do."
+fi
+
