ec3333912dae9f7203e859068c39c41ead26649b
BalderHolst <balderwh@gmail.com>
2023-12-25 22:40:06 +0100

    vault backup: 2023-12-25 22:40:06

--- install.sh
+++ install.sh
@@ -1,25 +1,37 @@
 #!/bin/sh
 
 dir=$(dirname $0)
-
 dir_name="notes"
+
+done_something=0
+
+# if script is run by itself
+[[ ! -d "$dir/.git" ]] && {
+    git clone git@github.com:BalderHolst/uni-notes "$dir_name"
+    dir="$dir/$dir_name"
+    done_something=1
+    echo "Cloned notes."
+}
 
 # if the repo exist
 [[ -d "$dir/.git" ]] && [[ ! -d "$dir/.obsidian" ]] && {
     git clone git@github.com:BalderHolst/uni-notes-settings "$dir/.obsidian"
     git -C "$dir" update-index --assume-unchanged ".obsidian"
+    done_something=1
     echo "Added settings."
-    exit 0
 }
 
-# if script is run by itself
-[[ ! -d "$dir/.git" ]] && {
-    git clone git@github.com:BalderHolst/uni-notes "$dir_name"
-    git clone git@github.com:BalderHolst/uni-notes-settings "$dir/$dir_name/.obsidian"
-    git -C "$dir/$dir_name" update-index --assume-unchanged ".obsidian"
-    echo "Cloned notes and added settings."
-    exit 0
+# Function for cloning classmates's vaults
+clone_classmate_vault ()
+{
+    [[ ! -d "$dir/" ]]
+    name="$1"
+    url="$2"
 }
 
-echo "Nothing to do."
-exit 0
+[[ "$1" = "--classmates" ]] && {
+    
+    done_something=1
+}
+
+[[ $done_something = 0 ]] && echo "Nothing to do."
